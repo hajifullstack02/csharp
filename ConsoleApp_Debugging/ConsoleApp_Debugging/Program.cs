c4b99f109ed6cc1030b3afa4e4fc5b6e08c8b198
hajifullstack@02 <haji.fullstack@gmail.com>
2025-11-23 21:56:43 +0530

    debugger

---
+++ ConsoleApp_Debugging/ConsoleApp_Debugging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+    {
+        static void Main()
+        {
+        printFirstCheckpoint();
+        }
+       static void printFirstCheckpoint()
+    {
+        Console.WriteLine("*****checkpoint one*****");
+        string studentname = "somu";
+        int studentage = 20;
+        decimal courseFee = 29999.9934M;
+        Console.WriteLine($"the student is {studentname} and age is {studentage} and coursefee is {courseFee}");
+        Console.WriteLine("get the location");
+       String getStudentLocation= Console.ReadLine();
+        Console.WriteLine($"student location is {getStudentLocation}");
+    }
+    }
+
