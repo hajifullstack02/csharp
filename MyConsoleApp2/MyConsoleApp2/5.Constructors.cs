ca40a03f362ce2209418e47d744975f848f9e411
hajifullstack@02 <haji.fullstack@gmail.com>
2025-11-26 08:05:02 +0530

    Constructors

---
+++ MyConsoleApp2/MyConsoleApp2/5.Constructors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class FileDetailsConstructors
+{
+
+    string filename = "Student1_Resume.docx";
+    string filelocation = @"D:\Students\Resumes";
+    string filesize = "2Kb";
+    string createddate = "Mar-01-2024";
+    DateTime createdDateTime = DateTime.Now;
+
+
+    public FileDetailsConstructors()
+    {
+        ///Code here..
+        ///
+        Console.WriteLine("File object created");
+        Console.WriteLine(this.filename); // "Student1_Resume.docx"
+
+        this.filename = "Sam_reusme.docx";
+
+    }
+
+    static void Main()
+    {
+        FileDetailsConstructors file1 = new FileDetailsConstructors();
+
+        //Note : i have changed the filename inside of the constrcutor.
+
+        Console.WriteLine(file1.filename);  //Sam_reusme.docx
+    }
+
+
+
+
+}
